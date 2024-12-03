517345dc139e8050d9d9e370858e30a3582f33cd
Bonthu Sai Lahari <laharibonthu1882@gmail.com>
2024-12-03 15:19:33 +0530

    commit

--- src/components/about-page.js
+++ src/components/about-page.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-
 import Navbar from "./navbar";
 import '../component-styles/about-page-styles.css';
 
 function LoadAbout() {
-
     useEffect(() => {
         document.title = 'About | StreamSwitch';
 
@@ -25,9 +23,10 @@ function LoadAbout() {
                     <div id="data">
                         <h1>What is StreamSwitch?</h1>
                         <p>
-                        StreamSwitch is an application that makes easy to move your playlists from Spotify to YouTube Music.  
-                        Out of the frustration with Tune My Music’s limit of 200 songs per transfer, I created this application
-                        As someone who loves music and often switches between platforms, I wanted something that worked better for people like me—and that’s how StreamSwitch was born.
+                            <span>StreamSwitch is an application that makes it easy to move your playlists from Spotify to YouTube Music.</span>
+                            <span>Out of the frustration with Tune My Music’s limit of 200 songs per transfer, I created this application.</span>
+                            <span>As someone who loves music and often switches between platforms, </span>
+                            <span>I wanted something that worked better for people like me—and that’s how StreamSwitch was born.</span>
                         </p>
 
                         <h1>Contact Me</h1>
@@ -35,7 +34,6 @@ function LoadAbout() {
                             <a name="Github" href="https://github.com/LahariBonthu"><i className="fa-brands fa-github"></i></a>
                             <a name="Linkedin" href="https://www.linkedin.com/in/bonthusailahari/"><i className="fa-brands fa-linkedin-in"></i></a>
                             <a name="Mail" href="mailto:laharibonthu1882@gmail.com"><i className="fa-solid fa-envelope"></i></a>
-                        
                         </div>
                     </div>
                 </div>
